96b6607962b9c7bb7cf5d21f2e84c152e6f1cec5
Dmitry-Poltaradnev <poltaradnev@gmail.com>
2024-06-26 19:07:42 +0400

    add: solve for hw1s2

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -14,7 +14,19 @@ import avatar from './avatar.png'
 * */
 
 // нужно создать правильный тип вместо any
-export type MessageType = any
+export type MessagesType ={
+    text: string
+    time: string
+}
+type UserType ={
+    avatar: string
+    name: string
+}
+export type MessageType = {
+    id: number
+    user: UserType
+    message: MessagesType
+}
 
 // структуру объекта не менять
 export const message0: MessageType = {
