fa192828e2a1a846ddc1c1163178a19261911df1
Dmitry-Poltaradnev <poltaradnev@gmail.com>
2024-08-16 14:19:28 +0400

    add: solve for hw2s3

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -5,28 +5,21 @@ import s2 from '../../s1-main/App.module.css'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s from './HW6.module.css'
 
-/*
- * 1 - в файле SuperEditableSpan.tsx дописать логику функций onEnterCallback, onBlurCallback, onDoubleClickCallBack
- * 2 - дописать логику функции restore
- * 3 - сделать стили в соответствии с дизайном
- */
-
 const HW6 = () => {
     const [value, setValue] = useState<string>('')
 
     const save = () => {
         saveState<string>('hw6-editable-span-value', value)
     }
-    const restore = () => {
-        // делают студенты
 
+    const restore = () => {
+        setValue(restoreState<string>('hw6-editable-span-value', ''))
     }
 
     return (
         <div id={'hw6'}>
             <div className={s2.hwTitle}>Homework #6</div>
 
-            {/*демонстрация возможностей компоненты:*/}
             <div className={s2.hw}>
                 <div className={s.editableSpanContainer}>
                     <SuperEditableSpan
