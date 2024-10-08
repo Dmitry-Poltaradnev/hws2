ed0527540eda0b5c1d3d1a389139457628d48606
Dmitry-Poltaradnev <poltaradnev@gmail.com>
2024-10-08 15:14:44 +0400

    add: solve for hw9s4

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,20 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+        case 'sort': {
+            const sorted = [...state].sort((a, b) => {
+                if (action.payload === 'up') {
+                    return a.name.localeCompare(b.name); // Сортировка по возрастанию
+                } else {
+                    return b.name.localeCompare(a.name); // Сортировка по убыванию
+                }
+            });
+            return sorted;
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(user => user.age >= action.payload);
         }
         default:
             return state
